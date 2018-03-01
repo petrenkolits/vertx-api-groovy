be0e9b7fdc4e039ee08b8dacb78b2435e60348ab
Viacheslav Petrenko <viacheslav.petrenko@litslink.com>
2018-03-01 17:23:29 +0200

    ES connection

--- src/main/groovy/MainVerticle.groovy
+++ src/main/groovy/MainVerticle.groovy
@@ -1,37 +1,84 @@
+import com.hubrick.vertx.elasticsearch.model.SearchResponse
 import groovy.transform.CompileStatic
 import io.vertx.core.Handler
+import io.vertx.core.Vertx
 import io.vertx.core.http.HttpServerResponse
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.BodyHandler
+import com.hubrick.vertx.elasticsearch.ElasticSearchService
+import com.hubrick.vertx.elasticsearch.model.SearchOptions
+import groovy.json.JsonOutput
 
 def options = [
   logActivity: true
 ]
 
 def router = Router.router(vertx)
+def eb = vertx.eventBus()
+def consumer = eb.consumer('eb')
+consumer.handler({ message ->
+  println("I have received a message: ${message.body()}")
+})
 router.route().handler(BodyHandler.create())
 
 @CompileStatic
 class Root implements Handler<RoutingContext> {
-  static Root create() {
+  static Root create(Vertx vtx) {
     println 'Creating instance'
-    new Root()
+    new Root(vtx)
   }
 
-  RoutingContext routingContext
+  ElasticSearchService elasticSearchService
 
-  HttpServerResponse getResponse() {
-    this.routingContext.response()
+  Root(Vertx vtx) {
+    this.elasticSearchService = ElasticSearchService.createEventBusProxy(vtx, 'eb')
+  }
+
+  void search(Handler handler) {
+    String query = JsonOutput.toJson([
+      query: [
+        bool: [
+          must: [
+            term: [
+              available: true
+            ]
+          ]
+        ]
+      ],
+      sort: [
+        [
+          _geo_distance: [
+            location_center: [
+              lat: "50.0000",
+              lon: "40.0000"
+            ],
+            order: "asc",
+            unit: "km",
+            distance_type: "plane"
+          ]
+        ]
+      ],
+      size: 1000
+    ])
+    SearchOptions searchOptions = new SearchOptions().setQuery(new JsonObject(query))
+    this.elasticSearchService.search("locations", searchOptions, handler)
   }
 
   void handle(RoutingContext context) {
-    this.routingContext = context
-    response.putHeader('Content-Type', 'text/plain')
-    response.end('XXXX')
+    def response = context.response()
+    def closure = { SearchResponse r ->
+      response.end(r.getHits().toString())
+    }
+    response.putHeader('Content-Type', 'application/json')
+    search {
+      println('xxxx')
+      closure.&call
+    }
   }
 }
 
-router.get('/').handler(Root.create())
+router.get('/').handler(Root.create(vertx))
 
 vertx.createHttpServer(options).requestHandler(router.&accept).listen(8080)
